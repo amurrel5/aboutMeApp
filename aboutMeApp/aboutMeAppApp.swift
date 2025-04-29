52718c4dc699ae56b932b6d07adf587f8c53ee9e
Ashley <anm38060@gmail.com>
2025-04-28 21:51:40 -0500

    Initial Commit

---
+++ aboutMeApp/aboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeAppApp.swift
+//  aboutMeApp
+//
+//  Created by Scholar on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
